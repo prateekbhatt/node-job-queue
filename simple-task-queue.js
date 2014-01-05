faebcfa081496a8b409712d47c6fefe3db802277
Prateek Bhatt <prattbhatt@gmail.com>
2014-01-05 13:16:55 +0530

    added job-specific events to simple-task-queue

--- simple-task-queue.js
+++ simple-task-queue.js
@@ -7,16 +7,22 @@ function newJob (name){
   var job = jobs.create('new job', {
     name: name
   });
+
+  job
+    .on('complete', function (){
+      console.log('Job', job.id, 'with name', job.data.name, 'is done');
+    })
+    .on('failed', function (){
+      console.log('Job', job.id, 'with name', job.data.name, 'has failed');
+    })
+
   job.save();
 }
 
 jobs.process('new job', function (job, done){
-  console.log('Job', job.id, 'with name', job.data.name, 'is done');
-
   /* carry out all the job function here */
-
   done && done();
-})
+});
 
 
 setInterval(function (){

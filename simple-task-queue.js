d6de5bbb033797fa4e4074ebab7e69ff4e4a1324
Prateek Bhatt <prattbhatt@gmail.com>
2014-01-05 10:06:02 +0530

    updated simple-task-queue

--- simple-task-queue.js
+++ simple-task-queue.js
@@ -2,15 +2,23 @@ var kue = require('kue')
   , jobs = kue.createQueue()
   ;
 
-function newJob (jobName){
-  var job = jobs.create('hello');
+function newJob (name){
+  name = name || 'Default_Name';
+  var job = jobs.create('new job', {
+    name: name
+  });
   job.save();
 }
 
-jobs.process('hello', function (job, done){
-  console.log('Job', job.id, 'is done');
+jobs.process('new job', function (job, done){
+  console.log('Job', job.id, 'with name', job.data.name, 'is done');
+
+  /* carry out all the job function here */
+
   done && done();
 })
 
 
-setInterval(newJob, 3000);
+setInterval(function (){
+  newJob('send Email');
+}, 3000);
